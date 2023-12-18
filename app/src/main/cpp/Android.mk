0594b8dcf47ae683ce89a6d7f2ec80eb06b0146f
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2023-12-18 00:34:56 +0000

    openssl + curl libs and headers included

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -2,20 +2,20 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ssl
-LOCAL_SRC_FILES := libs/openssl/$(TARGET_ARCH_ABI)/lib/libssl.a
-LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/libs/openssl/$(TARGET_ARCH_ABI)/include
+LOCAL_SRC_FILES := libs/openssl-android/$(TARGET_ARCH_ABI)/libssl.a
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := crypto
-LOCAL_SRC_FILES := libs/openssl/$(TARGET_ARCH_ABI)/lib/libcrypto.a
-LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/libs/openssl/$(TARGET_ARCH_ABI)/include
+LOCAL_SRC_FILES := libs/openssl-android/$(TARGET_ARCH_ABI)/libcrypto.a
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := curl
-LOCAL_SRC_FILES := libs/curl/$(TARGET_ARCH_ABI)/lib/libcurl.a
-LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/libs/curl/$(TARGET_ARCH_ABI)/include
+LOCAL_SRC_FILES := libs/curl-android/$(TARGET_ARCH_ABI)/libcurl.a
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
--- app/src/main/cpp/jni.c
+++ app/src/main/cpp/jni.c
@@ -3,9 +3,9 @@
 
 char *CA_BUNDLE_PATH;
 
-JNIEXPORT jboolean JNICALL
+JNIEXPORT jboolean
 Java_io_robertying_androidcurlexample_CurlHelper_curlAWebsite(JNIEnv *env,
-                                                              jobject this,
+                                                              jclass thiz,
                                                               jstring ca_bundle_path,
                                                               jstring url) {
     CA_BUNDLE_PATH = (char *) (*env)->GetStringUTFChars(env, ca_bundle_path, NULL);
@@ -15,9 +15,9 @@ Java_io_robertying_androidcurlexample_CurlHelper_curlAWebsite(JNIEnv *env,
     return success;
 }
 
-JNIEXPORT jstring JNICALL
+JNIEXPORT jstring
 Java_io_robertying_androidcurlexample_CurlHelper_getCurlVersion(JNIEnv *env,
-                                                              jobject this) {
+                                                                jclass thiz) {
     const char* versionChar = get_curl_version();
     return (*env)->NewStringUTF(env, versionChar);
 }
